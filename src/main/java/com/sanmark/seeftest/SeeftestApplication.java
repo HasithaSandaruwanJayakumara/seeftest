3661c61dff8d960c199b107bec81cbde3ab3031e
Hasitha Sandaruwan <hasitha@thesanmark.com>
2023-07-22 02:13:05 +0530

    initial

---
+++ src/main/java/com/sanmark/seeftest/SeeftestApplication.java
@@ -0,0 +1,13 @@
+package com.sanmark.seeftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeftestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeftestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanmark/seeftest/SeeftestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanmark.seeftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeftestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
